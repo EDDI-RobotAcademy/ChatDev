368b2aec218fe01e9dfdb27c3b633cc00ef46f87
onebottlekick <80615330+onebottlekick@users.noreply.github.com>
2024-10-08 11:44:13 +0900

    fix: [한병찬] Update run.sh[SIFA-0]

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ python runner.py --task "develop a simple calculator app"\
                  --project_name test_project\
                  --config Default\
                  --model_name llama3.2\
-                 --api_url https//si-follow.loca.lt
+                 --api_url "https://si-follow.loca.lt"
