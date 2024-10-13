d1d873dcc9a421ccfc044906a557260a72da8df7
onebottlekick <gksqudcks97@gmail.com>
2024-10-14 03:47:49 +0900

    fix: [한병찬] runner를 동시에 여러번 실행시켰을 때 log파일이 추가로 생성되지 않는 문제 해결[SIFA-21]

--- chatdev/logger.py
+++ chatdev/logger.py
@@ -5,17 +5,19 @@ class Logger:
     def __init__(self, log_file_name, logger_name, log_level=logging.INFO):
         self.__logger = logging.getLogger(logger_name)
         self.__logger.setLevel(log_level)
-        
-        if not self.__logger.hasHandlers():
-            formatter = logging.Formatter('[%(asctime)s] - [%(filename)s file line:%(lineno)d] - %(levelname)s: %(message)s')
-            
-            file_handler = logging.FileHandler(log_file_name)
-            file_handler.setFormatter(formatter)
-            self.__logger.addHandler(file_handler)
-            
-            console_handler = logging.StreamHandler()
-            console_handler.setFormatter(formatter)
-            self.__logger.addHandler(console_handler)
+
+        self.__logger.handlers = []
+        formatter = logging.Formatter(
+            "[%(asctime)s] - [%(filename)s file line:%(lineno)d] - %(levelname)s: %(message)s"
+        )
+
+        file_handler = logging.FileHandler(log_file_name)
+        file_handler.setFormatter(formatter)
+        self.__logger.addHandler(file_handler)
+
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        self.__logger.addHandler(console_handler)
 
     def get_logger(self):
         return self.__logger
