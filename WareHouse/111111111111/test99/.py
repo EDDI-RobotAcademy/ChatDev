fa3fb18c5f2e8b5bc59a813dea332fa2a230cd8c
gigcot <gigcot@gmail.com>
2024-10-14 17:09:52 +0900

    remove:[이재호] warehouse가 커밋내용에포함된걸삭제[SIFA-x]

--- WareHouse/111111111111/test99/.py
+++
@@ -1,5 +0,0 @@
-calculator_app/
-main.py
-utils.py
-calculator_gui.py
-__init__.py
--- WareHouse/111111111111/test99/calculator_gui.py
+++
@@ -1,5 +0,0 @@
-"""
-Graphical user interface for the calculator application.
-Defines the CalculatorGUI class, which handles user input and displays results.
-"""
-# This code is not needed as we have already implemented the GUI using tkinter in main.py
